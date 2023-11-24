fbe31a5d6bb55b20f20f161abf0c4c7c47535d4c
Sneha kumari <sneha2507kumari@gmail.com>
2023-11-24 18:30:09 +0530

    "Camera details"

--- bipolar/src/Components/Body/Body.jsx
+++ bipolar/src/Components/Body/Body.jsx
@@ -2,16 +2,45 @@ import React from 'react';
 import styles from "../Body/Body.module.css";
 import fetch from "../../Assets/fetch.png";
 import record from "../../Assets/record.png";
+import camera from "../../Assets/CameraDetails.png"
 
 const Body = () => {
   return (
     <div className={styles.bodymain}>
         <div className={styles.subbodymain}>
         <div className={styles.bodybutton}>
-            <button className={styles.bodybutton1}><img src={record}/>View Recorded Video</button>
-            <button className={styles.bodybutton1}><img src={fetch}/>Refetch Camera</button>
+            <button className={styles.bodybutton1}><img src={record} alt="record"/>View Recorded Video</button>
+            <button className={styles.bodybutton1}><img src={fetch} alt="fetch"/>Refetch Camera</button>
+          </div>
+        <div className={styles.subbodymain2}>
+          <div className={styles.camerapic}><img src={camera} alt="camera"/>
+          <div className={styles.camerapicDetails}>
+          <div className={styles.camerapicDetails2}>
+            <div className={styles.camerapicDetails3}>
+              <h1 className={styles.details}>Camera Details</h1>
+              <button className={styles.camerabutton}>Active</button>
+            </div>
+            <div className={styles.camerapicDetails4}>
+                <p className={styles.contentbody1}>Location:  <p className={styles.contentbody2}>Coimbatore</p></p>
+                <p className={styles.contentbody1}>City:  <p className={styles.contentbody2}>Coimbatore</p></p>
+                <p className={styles.contentbody1}>Timezone:  <p className={styles.contentbody2}>Delhi-India</p></p>
+                <p className={styles.contentbody1}>Date added  <p className={styles.contentbody2}>Aug 03,2023 12:01:42 PM</p></p>
+                <p className={styles.contentbody1}>Last update:  <p className={styles.contentbody2}>Aug 03,2023 12:01:42 PM</p></p>
+            
+            </div>
+            <div className={styles.camerapicDetails5}>
+                <p className={styles.contentbody1}>Manufacture details:  <p className={styles.contentbody2}>ADT</p></p>
+                <p className={styles.contentbody1}>RTSP/HLS URL:  <p className={styles.contentbody3}>Copy URL</p></p>
+            </div>
+            
+            
+            
+            </div>
+            </div>
+          </div>
         </div>
         </div>
+        
     </div>
   )
 }
